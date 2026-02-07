0659fde3a2c0f1d32743684ca3ebbc9f9a51a2d6
Ahmed <ahmed.elsayed862@yahoo.com>
2026-02-07 14:54:33 +0200

    MB automated Batchs

---
+++ Insert MB into our summited DB/MB_Batch.bat
@@ -0,0 +1,6 @@
+
+
+@echo off
+"C:\Users\Ahmad.elsayed\AppData\Local\Continuum\anaconda3\python.exe" "C:\AhmedElsayed-Reliable Work Space\Insert MB Data in our database\MB_Fetcher&&Inserted.py"
+Pause
+
