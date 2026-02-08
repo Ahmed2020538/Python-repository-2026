251a45ab80ebbc6dcacd930f90984ca0f2a09b91
Ahmed <ahmed.elsayed862@yahoo.com>
2026-02-08 02:49:17 +0200

    Handle EWI last  Indexes after Price adjustment Process batch and Description

---
+++ Handle Indexes After Price Adjustment Process Scripts/Handle_EWI_last_Indexes_after_adjustmentbatch.bat
@@ -0,0 +1,6 @@
+
+
+@echo off
+"C:\Users\Ahmad.elsayed\AppData\Local\Continuum\anaconda3\python.exe" "C:\AhmedElsayed-Reliable Work Space\Handle_EWI_last_Indexes_after_adjustment\Handle_EWI_last_Indexes_after_adjustment.py"
+Pause
+
