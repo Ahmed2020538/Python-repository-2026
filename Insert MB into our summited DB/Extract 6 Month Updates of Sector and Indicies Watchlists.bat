f6799cf977658a32d6f3e5b2909f732c529d60e3
Ahmed <ahmed.elsayed862@yahoo.com>
2026-02-08 03:01:47 +0200

    Extract data of indexes, sector, and all tickers of them from our DB Batch

---
+++ Insert MB into our summited DB/Extract 6 Month Updates of Sector and Indicies Watchlists.bat
@@ -0,0 +1,6 @@
+
+
+@echo off
+"C:\Users\Ahmad.elsayed\AppData\Local\Continuum\anaconda3\python.exe" "C:\AhmedElsayed-Reliable Work Space\Extract Data from DB\Extract_Indicies&Sectors\Extract 6 Month Updates of Sector and Indicies Watchlists.py"
+Pause
+
